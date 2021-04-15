5e8e0fb4b9308b06d0d5c101f758c8b8a4040e55
Mateusz <mateusz@com>
2021-04-15 19:02:06 +0200

    22.3 Task 22.3

---
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Trello {
+
+    @JsonProperty("board")
+    public int board;
+
+    @JsonProperty("card")
+    public int card;
+}
---
+++ src/main/java/com/crud/tasks/domain/TrelloAttachmentsByType.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloAttachmentsByType {
+
+    @JsonProperty("trello")
+    private Trello trello;
+}
