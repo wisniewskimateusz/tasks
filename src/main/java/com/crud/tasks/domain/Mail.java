138e6edb1167650ff1270b910bdcd2c564843720
Mateusz <mateusz@com>
2021-04-19 22:53:44 +0200

    23.3 Mail added.

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -9,5 +9,4 @@ public class Mail {
     private final String mailTo;
     private final String subject;
     private final String message;
-    private final String toCc;
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -31,7 +31,6 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        mailMessage.setCc(mail.getToCc());
         return mailMessage;
     }
 }
