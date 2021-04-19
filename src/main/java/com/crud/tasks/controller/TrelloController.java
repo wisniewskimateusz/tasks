9a2e08383ca68db8b00ea7a864333613ecc84e81
Mateusz <mateusz@com>
2021-04-19 22:06:32 +0200

    22.3 getTrelloBoards fix

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,6 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.client.TrelloClient;
 import com.sun.el.stream.Stream;
 import lombok.RequiredArgsConstructor;
@@ -13,25 +14,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
-@CrossOrigin
+@CrossOrigin("*")
 public class TrelloController {
 
     private final TrelloClient trelloClient;
+    private final TrelloService trelloService;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(
-                        trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()
-                );
-            });
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        //trelloService.fetchTrelloBoards();
+        return trelloClient.getTrelloBoards();
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//        trelloBoards.forEach(trelloBoardDto -> {
+//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+//
+//            System.out.println("This board contains lists: ");
+//
+//            trelloBoardDto.getLists().forEach(trelloList -> {
+//                System.out.println(
+//                        trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()
+//                );
+//            });
+//        });
     }
 
     @PostMapping("createTrelloCard")
